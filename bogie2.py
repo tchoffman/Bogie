ae56306d5e1220167a8f4d40072b3f407c0d456e
Trenton Hoffman <trenton.hoffman@gmail.com>
2015-07-29 00:34:32 +0000

    working on class Robot

---
+++ bogie2.py
@@ -0,0 +1,60 @@
+import XboxController
+import time
+import math
+import RPi.GPIO as gpio
+
+class Robot:
+    """
+    Class to setup a Robot.  Takes a dictionary of "commands" and
+    their associated output pins (BOARD number)
+    """
+    def __init__(self, outputs):
+        """
+        Initialize Output (and Input) pins. Assign objects to groups
+        """
+        gpio.setmode(gpio.BOARD)
+        self.outputs = outputs
+        self.drive = {}
+        self.sensor = {}
+        self.servo = {}
+        for output in self.outputs:
+            gpio.setup(self.outputs[output], gpio.OUT)     
+        for output in self.outputs.keys():
+            self.drive[output] = gpio.PWM(self.outputs[output],100)
+            self.drive[output].start(0)
+
+    def __repr__(self):
+        rep = "Robot("
+        rep += "Output Map: " + str(self.outputs) + ","
+        rep += "Drive: " + str(self.drive.keys()) + ","
+        rep += "Sensor: " + str(self.sensor.keys()) + ","
+        rep += "Servos: " + str(self.servo.keys()) + ")"
+        return rep
+
+    def halt(self):
+        for item in self.drive:
+            self.drive[item].stop()
+        gpio.cleanup()
+
+    
+        
+Bogie = Robot({"LR":7, "LF":11, "RR":13, "RF":15})
+        
+print Bogie
+
+try:
+    while True:
+        for i in range(0,101):
+            Bogie.drive["LF"].ChangeDutyCycle(i)
+            Bogie.drive["RF"].ChangeDutyCycle(i)
+            if i % 10 == 0:
+                print i, gpio.PWM(11)
+            time.sleep(0.02)
+        for i in range(0,101):
+            Bogie.drive["LF"].ChangeDutyCycle(100 - i)
+            Bogie.drive["RF"].ChangeDutyCycle(100 - i)
+            time.sleep(0.02)
+except KeyboardInterrupt:
+    for item in Bogie.drive:
+        Bogie.drive[item].stop()
+    gpio.cleanup()
--- xbox_test.py
+++ xbox_test.py
@@ -1,5 +1,6 @@
 import XboxController
 import time
+import math
 import RPi.GPIO as gpio
 
 
@@ -17,7 +18,7 @@ def init():
 def drive(controlId, value):
     print "Control id = {}, Value = {}".format(controlId, value)
     
-def monitor():
+def monitor1():
     command = ""
     if xboxCont.controlValues[1] < -0.1:
         command += "FORWARD, "
@@ -34,7 +35,39 @@ def monitor():
         return False
     print command
     return True
+
+def monitor2():
+    x_axis = xboxCont.controlValues[0]
+    y_axis = -1 * xboxCont.controlValues[1]
+    rotation = -math.pi/4
+    x_prime = (x_axis * math.cos(rotation)) - (y_axis * math.sin(rotation))
+    y_prime = (x_axis * math.sin(rotation)) + (y_axis * math.cos(rotation))
+    command = "Y: ", y_prime, " X: ", x_prime
+    if xboxCont.controlValues[12] == 1:
+        print "STOPPING"
+        xboxCont.stop()
+        gpio.cleanup
+        return False
     
+    print command
+    return True
+
+def monitor3():
+    fb_axis = xboxCont.controlValues[0]
+    lr_axis = -1 * xboxCont.controlValues[1]
+    angle = math.degrees(math.atan2(fb_axis, lr_axis))
+    magnitude = abs(fb_axis) + abs(lr_axis)
+    command =  "Angle: " + str(angle) + "Mag: " + str(magnitude)
+    if xboxCont.controlValues[12] == 1:
+        print "STOPPING"
+        xboxCont.stop()
+        gpio.cleanup
+        return False
+    
+    print command
+    return True
+
+
 xboxCont = XboxController.XboxController(
     controllerCallBack = None,
     joystickNo = 0,
@@ -46,4 +79,4 @@ init()
 xboxCont.start()
 run = True
 while run == True:
-    run = monitor()
+    run = monitor3()
